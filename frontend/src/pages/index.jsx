ffdc32a7341c3307e1c4bf07b39bf08741846b5e
iskanmr <iskan.mustamir@mail.ugm.ac.id>
2023-05-23 13:59:38 +0700

    remove stuff

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,45 +1,5 @@
-import { useState, useEffect } from "react";
-
 import DefaultLayout from "../layouts/DefaultLayout";
 
-import Button from "../components/utils/Button";
-
-import Styles from "../styles/pages/index.module.css";
-
 export default function Home() {
-  const [conState, setConState] = useState("No Monitoring!");
-  const [conStart, setConStart] = useState(false);
-
-  const start = () => {
-    !conStart ? setConState("Monitoring") : setConState("No Monitoring!");
-    setConStart(!conStart);
-  };
-
-  useEffect(() => {
-    const token =
-      typeof window !== "undefined" ? localStorage.getItem("token") : null;
-
-    if (!token) {
-      window.location.replace("/login");
-    }
-  });
-
-  return (
-    <DefaultLayout seoTitle="Home">
-      <div className={Styles.container}>
-        <div className={Styles.content}>
-          <img
-            src={`/assets/${
-              conState == "Monitoring" ? "save.svg" : "notMonitor.svg"
-            }`}
-            alt="Not Monitoring"
-          />
-          <h1 style={{ textAlign: "center" }}> {conState}</h1>
-        </div>
-        <Button type={conStart ? "active" : ""} onClick={start}>
-          {conStart ? "Stop monitoring" : "Start monitoring"}
-        </Button>
-      </div>
-    </DefaultLayout>
-  );
+  return <DefaultLayout seoTitle="Home">Home</DefaultLayout>;
 }
