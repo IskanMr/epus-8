46a6315aeb3f9eea9a7258814fc16c1b79319bfe
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-06-04 11:26:26 +0700

    add error handling

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -35,21 +35,39 @@ async def root():
 
 @app.post("/pengguna/")#,, response_model=PenggunaSchema)
 async def create_pengguna(pengguna: PenggunaSchema):
-    db_pengguna = PenggunaModel(username=pengguna.username, password=pengguna.password)
-    db.session.add(db_pengguna)
-    db.session.commit()
-    #get user_id values
-    print(db_pengguna.id_user)
-    return db_pengguna
+    try:
+        db_pengguna = PenggunaModel(username=pengguna.username, password=pengguna.password)
+        #check if username already exist
+        if db.session.query(PenggunaModel).filter(PenggunaModel.username == pengguna.username).first() is not None:
+            raise HTTPException(status_code=400, detail="Username already exist")
+        db.session.add(db_pengguna)
+        db.session.commit()
+        #get user_id values
+        print(db_pengguna.id_user)
+        return db_pengguna
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
+
 
 @app.post("/pengguna/login/", response_model=PenggunaWithIDSchema)
 async def login_pengguna(pengguna: PenggunaSchema):
-    db_pengguna = db.session.query(PenggunaModel).filter(PenggunaModel.username == pengguna.username).first()
-    if db_pengguna is None:
-        return {"message": "not found"}
-    if db_pengguna.password == pengguna.password:
-        return db_pengguna
-    return {"message": "wrong password"}
+    try:
+        db_pengguna = db.session.query(PenggunaModel).filter(PenggunaModel.username == pengguna.username).first()
+        if db_pengguna is None:
+            return {"message": "not found"}
+        if db_pengguna.password == pengguna.password:
+            return db_pengguna
+        return {"message": "wrong password"}
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/pengguna/logout/")
 async def logout_pengguna(pengguna: PenggunaSchema):
@@ -61,30 +79,51 @@ async def logout_pengguna(pengguna: PenggunaSchema):
 
 @app.delete("/pengguna/{id_user}")
 async def delete_pengguna(id_user: int):
-    db_pengguna = db.session.query(PenggunaModel).filter(PenggunaModel.id_user == id_user).first()
-    #if not found
-    if db_pengguna is None:
-        return {"message": "not found"}
-    db.session.delete(db_pengguna)
-    db.session.commit()
-    return db_pengguna
+    try:
+        db_pengguna = db.session.query(PenggunaModel).filter(PenggunaModel.id_user == id_user).first()
+        #if not found
+        if db_pengguna is None:
+            return {"message": "not found"}
+        db.session.delete(db_pengguna)
+        db.session.commit()
+        return db_pengguna
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/tempat/")#,, response_model=TempatSchema)
 async def create_tempat(tempat: TempatSchema):
-    db_tempat = TempatModel(nama_tempat=tempat.nama_tempat)
-    db.session.add(db_tempat)
-    db.session.commit()
-    print(db_tempat.id_place)
-    return db_tempat
-
+    try:
+        db_tempat = TempatModel(nama_tempat=tempat.nama_tempat)
+        db.session.add(db_tempat)
+        db.session.commit()
+        print(db_tempat.id_place)
+        return db_tempat
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
+        
 @app.delete("/tempat/{id_place}")
 async def delete_tempat(id_place: int):
-    db_tempat = db.session.query(TempatModel).filter(TempatModel.id_place == id_place).first()
-    if db_tempat is None:
-        return {"message": "not found"}
-    db.session.delete(db_tempat)
-    db.session.commit()
-    return db_tempat
+    try:
+        db_tempat = db.session.query(TempatModel).filter(TempatModel.id_place == id_place).first()
+        if db_tempat is None:
+            return {"message": "not found"}
+        db.session.delete(db_tempat)
+        db.session.commit()
+        return db_tempat
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @app.get("/tempat/")
 async def get_all_tempat():
@@ -92,11 +131,18 @@ async def get_all_tempat():
 
 @app.post("/waktu_tersedia/")#, response_model=Waktu_TersediaSchema)
 async def create_waktu_tersedia(waktu_tersedia: Waktu_TersediaSchema):
-    db_waktu_tersedia = Waktu_TersediaModel(id_place=waktu_tersedia.id_place, is_available=waktu_tersedia.is_available, waktu=waktu_tersedia.waktu)
-    db.session.add(db_waktu_tersedia)
-    db.session.commit()
-    print(db_waktu_tersedia.id)
-    return db_waktu_tersedia
+    try:
+        db_waktu_tersedia = Waktu_TersediaModel(id_place=waktu_tersedia.id_place, is_available=waktu_tersedia.is_available, waktu=waktu_tersedia.waktu)
+        db.session.add(db_waktu_tersedia)
+        db.session.commit()
+        print(db_waktu_tersedia.id)
+        return db_waktu_tersedia
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @app.get("/waktu_tersedia/{id_place}")
 async def get_waktu_tersedia(id_place: int):
@@ -104,37 +150,58 @@ async def get_waktu_tersedia(id_place: int):
 
 @app.delete("/waktu_tersedia/{id_waktu}")
 async def delete_waktu_tersedia(id_waktu: int):
-    db_waktu_tersedia = db.session.query(Waktu_TersediaModel).filter(Waktu_TersediaModel.id == id_waktu).first()
-    if db_waktu_tersedia is None:
-        return {"message": "not found"}
-    db.session.delete(db_waktu_tersedia)
-    db.session.commit()
-    return {"message": "deleted"}
+    try:
+        db_waktu_tersedia = db.session.query(Waktu_TersediaModel).filter(Waktu_TersediaModel.id == id_waktu).first()
+        if db_waktu_tersedia is None:
+            return {"message": "not found"}
+        db.session.delete(db_waktu_tersedia)
+        db.session.commit()
+        return {"message": "deleted"}
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/booking_waktu/")#,, response_model=Booking_WaktuSchema)
 async def create_booking_waktu(booking_waktu: Booking_WaktuSchema):
-    db_booking_waktu = Booking_WaktuModel(id_user=booking_waktu.id_user, id_waktu=booking_waktu.id_waktu)
-    db.session.add(db_booking_waktu)
-    db.session.commit()
-    #set waktu tersedia menjadi tidak tersedia
-    db_waktu_tersedia = db.session.query(Waktu_TersediaModel).filter(Waktu_TersediaModel.id == booking_waktu.id_waktu).first()
-    db_waktu_tersedia.is_available = False
-    db.session.commit()
-    print(db_booking_waktu.id)
-    return db_booking_waktu
+    try:
+        db_booking_waktu = Booking_WaktuModel(id_user=booking_waktu.id_user, id_waktu=booking_waktu.id_waktu)
+        db.session.add(db_booking_waktu)
+        db.session.commit()
+        #set waktu tersedia menjadi tidak tersedia
+        db_waktu_tersedia = db.session.query(Waktu_TersediaModel).filter(Waktu_TersediaModel.id == booking_waktu.id_waktu).first()
+        db_waktu_tersedia.is_available = False
+        db.session.commit()
+        print(db_booking_waktu.id)
+        return db_booking_waktu
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @app.delete("/booking_waktu/{id_booking_waktu}")
 async def delete_booking_waktu(id_booking_waktu: int):
-    #set dahulu waktu tersedia menjadi tersedia
-    db_booking_waktu = db.session.query(Booking_WaktuModel).filter(Booking_WaktuModel.id == id_booking_waktu).first()
-    db_waktu_tersedia = db.session.query(Waktu_TersediaModel).filter(Waktu_TersediaModel.id == db_booking_waktu.id_waktu).first()
-    db_waktu_tersedia.is_available = True
-    db.session.commit()
-    #delete booking waktu
-    db.session.query(Booking_WaktuModel).filter(Booking_WaktuModel.id == id_booking_waktu).delete()
-    db.session.commit()
- 
-    return {"message": "deleted"}
+    try:
+        #set dahulu waktu tersedia menjadi tersedia
+        db_booking_waktu = db.session.query(Booking_WaktuModel).filter(Booking_WaktuModel.id == id_booking_waktu).first()
+        db_waktu_tersedia = db.session.query(Waktu_TersediaModel).filter(Waktu_TersediaModel.id == db_booking_waktu.id_waktu).first()
+        db_waktu_tersedia.is_available = True
+        db.session.commit()
+        #delete booking waktu
+        db.session.query(Booking_WaktuModel).filter(Booking_WaktuModel.id == id_booking_waktu).delete()
+        db.session.commit()
+    
+        return {"message": "deleted"}
+    except Exception as e:
+        print(e)
+        if e is HTTPException:
+            raise HTTPException(status_code=400, detail=e.detail)
+        else:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @app.get("/booking_waktu/user/{id_user}")
 async def get_booking_waktu(id_user: int):
