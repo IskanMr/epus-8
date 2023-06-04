04c90bc30bafa1db6d6ff7978b67981c139f08d1
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-06-04 19:16:53 +0700

    request ciwi2

--- backend/models/models.py
+++ backend/models/models.py
@@ -72,25 +72,72 @@ class Booking_Waktu(Base):
     #time_created = Column(DateTime(timezone=True), server_default=func.now())
     #time_updated = Column(DateTime(timezone=True), onupdate=func.now())
 
-class Buku(Base):
-    __tablename__ = 'buku'
-    id_buku = Column(Integer, primary_key=True)
-    judul_buku = Column(VARCHAR(128))
-    penulis = Column(VARCHAR(128))
-    cover = Column(VARCHAR(128))
-    stock = Column(Integer)
 
-class PinjamBuku(Base):
-    __tablename__ = 'pinjam_buku'
-    id_pinjam = Column(Integer, primary_key=True)
-    id_user = Column(Integer, ForeignKey('pengguna.id_user'))
-    user = relationship('Pengguna')
-    id_buku = Column(Integer, ForeignKey('buku.id_buku'))
-    buku = relationship('Buku')
+class Administrator(Base):
+    __tablename__ = 'administrator'
+    id_user = Column(Integer, primary_key=True)
+    kode_user = Column(VARCHAR(255))
+    nis = Column(VARCHAR(255))
+    fullname = Column(VARCHAR(255))
+    username = Column(VARCHAR(255))
+    password = Column(VARCHAR(255))
+    kelas = Column(VARCHAR(255))
+    alamat = Column(VARCHAR(255))
+    verif = Column(VARCHAR(255))
+    role = Column(VARCHAR(255))
+    join_date = Column(VARCHAR(255))
+    terakhir_login = Column(VARCHAR(255))
 
-class admin(Base):
-    __tablename__ = 'admin'
+class Authors(Base):
+    __tablename__ = 'authors'
     id = Column(Integer, primary_key=True)
-    full_name = Column(VARCHAR(128))
-    email = Column(VARCHAR(128))
-    password = Column(VARCHAR(128))
+    name = Column(VARCHAR(255))
+
+class Books(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column(VARCHAR(255))
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    author = relationship('Authors')
+    description = Column(VARCHAR(255))
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    cover = Column(VARCHAR(255))
+    file = Column(VARCHAR(255))
+    stock = Column(Integer, default=1)
+    year_published = Column(VARCHAR(255))
+    isbn = Column(Integer)
+    publisher_id = Column(Integer, ForeignKey('publisher.id_penerbit'))
+
+class Categories(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(255))
+
+class Publisher(Base):
+    __tablename__ = 'publisher'
+    id_penerbit = Column(Integer, primary_key=True)
+    kode_penerbit = Column(VARCHAR(255))
+    nama_penerbit = Column(VARCHAR(255))
+    verif_penerbit = Column(VARCHAR(255))
+
+class Identity(Base):
+    __tablename__ = 'identity'
+    id_identitas = Column(Integer, primary_key=True)
+    nama_app = Column(VARCHAR(255))
+    alamat_app = Column(VARCHAR(255))
+    email_app = Column(VARCHAR(255))
+    nomor_hp = Column(VARCHAR(255))
+
+class TbTransaksi(Base):
+    __tablename__ = 'tb_transaksi'
+    id_transaksi = Column(Integer, primary_key=True)
+    id_books = Column(Integer, ForeignKey('books.id'))
+    id_user = Column(Integer, ForeignKey('pengguna.id_user'))
+    tgl_pinjam = Column(VARCHAR(255))
+    tgl_kembali = Column(VARCHAR(255))
+    status = Column(VARCHAR(255))
+
+    
+
+
+
