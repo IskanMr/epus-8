46d8fa80be561687cb400a8089d0d1b07b6ebcbb
fajarrw <frizkywidodo@gmail.com>
2023-06-05 06:16:44 +0700

    change api protocol back to http

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setCurrentURL(window.location.href);
-      setApiEndpoint('https://20.51.177.188:1945/' + 'pengguna/login/');
+      setApiEndpoint('http://20.51.177.188:1945/' + 'pengguna/login/');
     }
     const token =
       typeof window !== "undefined" ? localStorage.getItem("token") : null;
